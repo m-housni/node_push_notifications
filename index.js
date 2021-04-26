7146eb8fba9716860edee2d472a7c26ab387de9c
HOUSNI Mohamed <mohamed.housni@um5s.net.ma>
2021-04-26 19:10:48 +0000

    commented the code

--- index.js
+++ index.js
@@ -1,19 +1,42 @@
+
+/*
+Express.js, or simply Express, is a back end web application framework for Node.js, released as free and open-source software under the MIT License. It is designed for building web applications and APIs. It has been called the de facto standard server framework for Node.js
+*/
 const express = require("express");
+
+/*
+Web push requires that push messages triggered from a backend be done via the Web Push Protocol and if you want to send data with your push message, you must also encrypt that data according to the Message Encryption for Web Push spec.
+*/
 const webpush = require("web-push");
+
+/* 
+Node.js body parsing middleware.
+Parse incoming request bodies in a middleware before your handlers, available under the req.body property.
+*/
 const bodyParser = require("body-parser");
+
+/*
+The Path module provides a way of working with directories and file paths
+*/
 const path = require("path");
 
+// Creates an Express application. The express() function is a top-level function exported by the express module.
 const app = express();
 
-// Set static path
+// Set static directory toserve images, CSS files, and JavaScript files named: client
 app.use(express.static(path.join(__dirname, "client")));
 
+// 
 app.use(bodyParser.json());
 
-const publicVapidKey =
-  "BJthRQ5myDgc7OSXzPCMftGw-n16F7zQBEN7EUD6XxcfTTvrLGWSIG7y_JxiWtVlCFua0S8MTB5rPziBqNx1qIo";
+// generate keys by typing 
+// ./node_modules/.bin/web-push generate-vapid-keys
+// in the terminal
+const publicVapidKey ="BJthRQ5myDgc7OSXzPCMftGw-n16F7zQBEN7EUD6XxcfTTvrLGWSIG7y_JxiWtVlCFua0S8MTB5rPziBqNx1qIo";
 const privateVapidKey = "3KzvKasA2SoCxsp0iIG_o9B0Ozvl1XDwI63JRKNIWBM";
 
+
+// set Vapi details
 webpush.setVapidDetails(
   "mailto:test@test.com",
   publicVapidKey,
